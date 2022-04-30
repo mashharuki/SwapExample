6816f728a19ef5d2cefd886b75b13e32053b6321
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-04-30 13:10:57 +0900

    追加

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,52 @@
 import { SwapWidget } from '@uniswap/widgets';
 import React, { useEffect, useState } from "react";
+import { providers, ethers } from 'ethers';
 import './App.css';
 import '@uniswap/widgets/fonts.css'
 import detectEthereumProvider from "@metamask/detect-provider";
 
-// Infura endpoint
-const jsonRpcEndpoint = process.env.REACT_APP_API_ENDPOINT;
-
 function App() {
+
+  // Infura endpoint
+  const jsonRpcEndpoint = process.env.REACT_APP_API_ENDPOINT;
+  const jsonRpcProvider = new providers.JsonRpcProvider(jsonRpcEndpoint);
+  // create provider
+  const provider = new ethers.providers.Web3Provider(jsonRpcProvider);
+  // ステート変数
+  const [account, setAccount] = useState({
+    address: '',
+    provider: provider,
+  })
+
+  async function connectWallet() {
+    const ethereumProvider = await detectEthereumProvider();
+
+    if (ethereumProvider) {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+
+      const address = accounts[0];
+      setAccount({
+        address: address,
+        provider: ethereumProvider
+      })
+    }
+  }
+
   return (
-    <div className="Uniswap">
-      <SwapWidget
-            provider={detectEthereumProvider}
-            jsonRpcEndpoint={jsonRpcEndpoint}
-      />
+    <div className="App">
+      <div>
+        <button onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      </div>
+      <div className="Uniswap">
+        <SwapWidget
+              provider={account.provider}
+              jsonRpcEndpoint={jsonRpcEndpoint}
+        />
+      </div>
     </div>
   );
 }
